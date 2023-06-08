32ac3e61388965f4b24898578efc32bf46a8a131
ddulyas <120781735+ddulyas@users.noreply.github.com>
2023-06-08 22:37:56 +0500

    create components for settingspage

--- src/components/ChatApp/components/Settings/Settings.tsx
+++ src/components/ChatApp/components/Settings/Settings.tsx
@@ -1,12 +1,38 @@
-import { FC } from "react";
-
+import { FC, MouseEvent, useEffect, useRef, useState } from "react";
+import style from './settings.module.scss'
+import { Context } from "../../../../main";
+import { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import useStopPropagation from "../../../../hooks/useStopPropagation";
+import EditRow from "./components/EditRow/EditRow";
 
 const Settings: FC = () => {
+
+    const { store } = useContext(Context)
+    const [showEditUserName, setShowEditUserName] = useState<boolean>(false)
+
+
+    const editUserName = (userName: string | undefined) => {
+        console.log(userName)
+        // e.stopPropagation()
+    }
+
+    const outClick = (e: MouseEvent) => {
+        e.stopPropagation()
+        console.log('outclick');
+        
+        setShowEditUserName(false)
+    }
+
+
+
     return (
-        <div>
-            Settings
+        <div 
+        onClick={outClick}
+        className={style.wrap}>
+            <EditRow placeholder={store.user.name ?? store.user.email} editValueFunction={editUserName} setShowButton={setShowEditUserName} showButton={showEditUserName}/>
         </div>
     );
 };
 
-export default Settings;
+export default observer(Settings);
---
+++ src/hooks/useStopPropagation.ts
@@ -0,0 +1,5 @@
+import { MouseEvent } from "react";
+
+export default function(e: MouseEvent) {
+    e.stopPropagation()
+}
